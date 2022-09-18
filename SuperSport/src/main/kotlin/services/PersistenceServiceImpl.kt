7b8b587de5e8a7d85784f77407db461aebcb1781
Mulisa Ramukosi <mulisa0998@gmail.com>
2022-09-18 12:00:12 +0200

    removed injections because they have not been registered yet

--- SuperSport/src/main/kotlin/services/PersistenceServiceImpl.kt
+++ SuperSport/src/main/kotlin/services/PersistenceServiceImpl.kt
@@ -1,20 +1,8 @@
 package services
 
 import models.CleanSiteScrap
-import repositories.*
-import uy.kohesive.injekt.Injekt
-import uy.kohesive.injekt.api.get
 
-class PersistenceServiceImpl(
-    private val commentaryRepo: CommentaryRepo = Injekt.get(),
-    private val eventRepo: EventRepo = Injekt.get(),
-    private val lineUpRepo: LineUpRepo = Injekt.get(),
-    private val locationRepo: LocationRepo = Injekt.get(),
-    private val matchRepo: MatchRepo = Injekt.get(),
-    private val playerRepo: PlayerRepo = Injekt.get(),
-    private val statRepo: StatRepo = Injekt.get(),
-    private val teamRepo: TeamRepo = Injekt.get()
-) : PersistenceService {
+class PersistenceServiceImpl : PersistenceService {
 
     override suspend fun save(cleanSiteScrap: CleanSiteScrap) {
         TODO("Not yet implemented")
